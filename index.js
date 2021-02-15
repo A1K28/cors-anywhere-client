a865c6b6c03b93bf98e69e5f69b4bb12145405fa
aleks <aleksandrekhorbaladze@gmail.com>
2021-02-16 02:58:48 +0400

    revert last commit

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var port = process.env.PORT || 8080;
  
 var cors_proxy = require('cors-anywhere');
 cors_proxy.createServer({
-    originWhitelist: ['a1k28.github.io/name-to-gender-classifier'],
+    originWhitelist: ['https://a1k28.github.io/name-to-gender-classifier'],
     requireHeader: ['origin', 'x-requested-with'],
     removeHeaders: ['cookie', 'cookie2']
 }).listen(port, host, function() {
